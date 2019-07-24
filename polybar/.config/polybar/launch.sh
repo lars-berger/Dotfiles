2518fc46b0b628041c9649f411bb2d084976a115
Lars Berger <lars.berger@yahoo.com>
2019-07-25 00:11:03 +0200

    polybar: renamed `config` to `config.ini` for syntax highlighting

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,5 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch main bar
-polybar -r main
+# Launch main bar with custom config path & live reloading
+polybar -r -c ~/.config/polybar/config.ini main &
