9208288d4baad51a798e83a8403fe6029bb25189
Lars Berger <lars.berger@yahoo.com>
2019-07-29 01:18:59 +0200

    shell: moved variables in `.profile` to `env.sh`

--- shell/.bash.d/env.sh
+++ shell/.bash.d/env.sh
@@ -2,3 +2,10 @@
 export YAOURT_COLORS="nb=1:pkg=1:ver=1;32:lver=1;45:installed=1;42:grp=1;34:od=1;41;5:votes=1;44:dsc=0:other=1;35"
 
 # export QT_SELECT=4
+
+export QT_QPA_PLATFORMTHEME="qt5ct"
+export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
+
+export EDITOR=/usr/bin/nano
+export BROWSER=/usr/bin/firefox
+export TERMINAL=/usr/bin/urxvt
