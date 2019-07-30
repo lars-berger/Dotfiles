3c2e7763b780b9657d589c34ffc39b865bece7f3
Lars Berger <lars.berger@plejd.com>
2019-07-30 16:56:39 +0200

    polybar: bar is launched on all displays

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,5 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch main bar with custom config path & live reloading
-polybar -r -c ~/.config/polybar/config.ini main &
+# Launch main bar on all displays
+# Use custom config config path & live reloading
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar -r -c ~/.config/polybar/config.ini main &
+done
