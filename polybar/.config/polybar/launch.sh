6b74fda061dfc1d1cad63bc4660e8b4757147395
Lars Berger <lars.berger@plejd.com>
2019-08-01 17:00:53 +0200

    polybar: display tray on primary output

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,8 +6,21 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+outputs=$(xrandr | grep " connected" | cut -d" " -f1)
+
+primary_output=$(xrandr | grep " connected primary" | cut -d " " -f1) || eDP1
+
 # Launch main bar on all displays
-# Use custom config config path & live reloading
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar -r -c ~/.config/polybar/config.ini main &
+for output in $outputs; do
+  export MONITOR=$output
+  export TRAY_POSITION=none
+
+  # Display tray on primary output
+  if [[ $output == $primary_output ]]; then
+    TRAY_POSITION=right
+  fi
+
+  # Launch with custom config path & live reloading
+  polybar -r -c ~/.config/polybar/config.ini main &
+  disown
 done
