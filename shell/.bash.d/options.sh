74c4ddb1a50d81bf74639a111acabfcb22a32527
Lars Berger <lars.berger@yahoo.com>
2019-08-06 16:31:19 +0200

    shell: make autocompletion case-insensitive

--- shell/.bash.d/options.sh
+++ shell/.bash.d/options.sh
@@ -1,10 +1,13 @@
 # Bash won't get SIGWINCH if another process is in the foreground.
 # Enable checkwinsize so that bash will check the terminal size when
-# it regains control.  #65623
+# it regains control.
 # http://cnswww.cns.cwru.edu/~chet/bash/FAQ (E11)
 shopt -s checkwinsize
 
 shopt -s expand_aliases
 
-# Enable history appending instead of overwriting.  #139609
+# Enable history appending instead of overwriting.
 shopt -s histappend
+
+# Make autocompletion case-insensitive
+bind 'set completion-ignore-case on'
