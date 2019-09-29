fbe50161604e2c4d2b77cead35abfa1316d38419
Lars Berger <lars.berger@yahoo.com>
2019-09-30 00:57:53 +0200

    shell: added aliasesfor 'npm run' and echoing PATH variable

--- shell/.bash.d/aliases.sh
+++ shell/.bash.d/aliases.sh
@@ -3,9 +3,13 @@ alias df='df -h'                          # human-readable sizes
 alias free='free -m'                      # show sizes in MB
 alias np='nano -w PKGBUILD'
 alias more=less
+alias r='npm run'
 
-alias l="ls -la"       # List in long format, include dotfiles
+alias l="ls -la --group-directories-first"       # List in long format, include dotfiles
 alias ld="ls -ld */"   # List in long format, only directories
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
+
+# Pretty print the PATH variable
+alias path='echo $PATH | tr -s ":" "\n"'
