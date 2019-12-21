36a7586d18ebc77ec2fb8bc885a6fdd26d5d805a
Lars Berger <lars.berger@yahoo.com>
2019-12-22 00:05:34 +0100

    shell: added environment variables necessary for .NET core

--- shell/.bash.d/env.sh
+++ shell/.bash.d/env.sh
@@ -6,3 +6,6 @@ export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 export EDITOR=/usr/bin/nvim
 export BROWSER=/usr/bin/firefox
 export TERMINAL=/usr/bin/urxvt
+
+export VDPAU_DRIVER=nouveau
+export MSBuildSDKsPath="/opt/dotnet/sdk/3.1.100/Sdks/";
