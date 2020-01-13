35416b48aee8cc7a8e3a97e41112d55ca53d926a
Lars Berger <lars.berger@plejd.com>
2020-01-13 11:06:04 +0100

    feat(bin): created shell script to run yarn/npm depending on whether yarn.lock is present

--- shell/.bash.d/aliases.sh
+++ shell/.bash.d/aliases.sh
@@ -3,7 +3,7 @@ alias df='df -h'                          # human-readable sizes
 alias free='free -m'                      # show sizes in MB
 alias np='nano -w PKGBUILD'
 alias more=less
-alias r='npm run'
+alias r='node-run'
 
 alias l="ls -la --group-directories-first"       # List in long format, include dotfiles
 alias ld="ls -ld */"   # List in long format, only directories
